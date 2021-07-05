e35dd8a3b5504438334e26973351fc0c22c868a5
Loc.DaoT <loc.daot@homecredit.vn>
2021-07-05 21:24:12 +0700

    Commit 4th

--- src/test/java/com/pluralsight/bdd/Calculator.java
+++ src/test/java/com/pluralsight/bdd/Calculator.java
@@ -2,6 +2,7 @@ package com.pluralsight.bdd;
 // Test commit 2rd
 // Test commit 3rd
 // Test commit 4th
+// Test commit 5th
 public class Calculator {
     public Integer add(Integer a, Integer b) {
         return a + b;
